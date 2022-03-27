866abf0ed3b9ac759d10abd3ff01c4091460d7b3
bollinca <51376777+bollinca@users.noreply.github.com>
2022-03-27 15:08:40 -0400

    Make objects persistent between scenes
    - prevent destruction of inventory on load
    - prevent destruction of boundaries on load

--- Programming Theory Project/Assets/Scripts/InventoryController.cs
+++ Programming Theory Project/Assets/Scripts/InventoryController.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class InventoryController : MonoBehaviour
 {
+
+    GameObject boundaries;
+    GameObject inventory;
     // Start is called before the first frame update
     void Start()
     {
-        
+        inventory = GameObject.Find("Inventory");
+        boundaries = GameObject.Find("Boundaries");
+        DontDestroyOnLoad(inventory);
+        DontDestroyOnLoad(boundaries);
     }
 
     // Update is called once per frame
